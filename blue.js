2875fd98852f1e288b567221fb36799f50e34ba3
George Kochera <kocherag@oregonstate.edu>
2021-02-28 19:43:22 -0500

    added some graphics for clarity and comments in the JS

--- blue.js
+++ blue.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// This is the route after what was looked for in app.use.
 router.get('/blue', function(req, res) {
     res.send('<body bgcolor="#CCCCFF"><h1>Hello! I am the <style color=blue>Blue</style> Page</h1><p><a href="/red">Red</a></p><p><a href="/blue">Blue</a></p><p><a href="/">Home</a></p></body>');
 })
--- main.js
+++ main.js
@@ -4,13 +4,16 @@ var express = require('express');
 var app = express();
 
 // ROUTES
-var blue = require('./blue');
+var blue = require('./blue'); // These are each basically imports of the other files, you don't need the js file extension
 var red = require('./red');
 var home = require('./home');
 
-app.use('/', blue);
+app.use('/', blue); // We are saying in each of these that any route starting with /, to use these special files
 app.use('/', red);
 app.use('/', home);
+
+// If I had app.use('/test', blue) in this file, and blue.js has a router.get('/blue'), then I would have to enter "localhost:8989/test/blue" in
+// my browser to access the page. They basically concatenate.
 
 // LISTENER
 app.listen(PORT, function(){
